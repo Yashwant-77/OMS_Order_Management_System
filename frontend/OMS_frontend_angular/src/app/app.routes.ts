1f48e665dedb35713d9e21b74c827aa882e3eb77
Yashwant-Chouhan <chouhanyashwant98@gmail.com>
2026-04-02 18:00:12 +0530

    only ui part of login page is completed

--- frontend/OMS_frontend_angular/src/app/app.routes.ts
+++ frontend/OMS_frontend_angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "",
+        component: Login,
+        pathMatch : "full"
+    }
+];
