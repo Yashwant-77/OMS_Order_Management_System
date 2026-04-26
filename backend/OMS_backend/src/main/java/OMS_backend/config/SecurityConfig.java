ab9f8cc29c7f6d6f9a23e026543e80daaf648bb5
Yashwant-Chouhan <chouhanyashwant98@gmail.com>
2026-04-26 12:23:54 +0530

    till now stable version to deploy ready

--- backend/OMS_backend/src/main/java/OMS_backend/config/SecurityConfig.java
+++ backend/OMS_backend/src/main/java/OMS_backend/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://oms-order-management-system-frontend.onrender.com" , "http://localhost:3000" ,"http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
